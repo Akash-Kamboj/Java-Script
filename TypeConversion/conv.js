0d6ec8ef56ce8f43f252a84982f4ab81558ecc03
Akash-Kamboj <akashkamboj632@gmail.com>
2025-06-04 15:07:14 +0530

    conversion done

---
+++ TypeConversion/conv.js
@@ -0,0 +1,17 @@
+let age = "23abc";
+
+console.log(typeof age);
+
+let ageNum = Number(age);
+console.log(typeof ageNum);
+
+console.log(ageNum);
+
+// ---------------------------------------------------------------------------------
+
+let score = null;
+console.log(typeof score);
+
+let scoreNum = Number(score);
+console.log(typeof scoreNum);
+console.log(scoreNum);
