a486527a153a05ef372a35c7a226e470ff90fb10
Akash-Kamboj <akashkamboj632@gmail.com>
2025-06-04 14:23:28 +0530

    variables done

---
+++ Variables/var.js
@@ -0,0 +1,13 @@
+const age = 25;
+let rollno = "1001";
+var city = "New York";
+
+let gender;
+
+// This will cause an error because 'age' is a constant
+// age  = 30;
+
+rollno = "1002";
+city = "Los Angeles";
+
+console.table([{ age }, { rollno }, { city }, { gender }]);
