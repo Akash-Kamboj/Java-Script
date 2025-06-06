2038b99d5848142a1a1bc6d1ded9beff90e92aa4
Akash-Kamboj <akashkamboj632@gmail.com>
2025-06-06 15:15:04 +0530

    Date Done

---
+++ Date/date.js
@@ -0,0 +1,18 @@
+let date = new Date();
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toISOString());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString());
+console.log(date.toLocaleString());
+console.log(date.getFullYear());
+console.log(date.getMonth());
+
+let mydate = Date.now();
+console.log(Math.floor(mydate / 1000));
+
+let mydate2 = new Date();
+console.log(mydate2.getDate());
+
+
